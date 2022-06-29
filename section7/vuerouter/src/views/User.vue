7606f1fa468827ab76b6db9cce9236f106895710
hanada <90606895+mitutaka@users.noreply.github.com>
2022-06-29 16:39:47 +0900

    First commit

---
+++ section7/vuerouter/src/views/User.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    ユーザー情報<br />
+    <div class="green-b">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.green-b {
+  border: 1px solid green;
+}
+</style>
