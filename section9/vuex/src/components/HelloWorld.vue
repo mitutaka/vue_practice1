87cf600893818d6fc9fc94c4ca7bacb4483ab3be
hanada <90606895+mitutaka@users.noreply.github.com>
2022-07-05 00:09:57 +0900

    section9.Initial commit

---
+++ section9/vuex/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button @click="addCount">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  methods: {
+    addCount() {
+      this.$store.commit("increment");
+    },
+  },
+};
+</script>
---
+++ section9/vuex/src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    // 初期値
+    count: 0,
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+  },
+  actions: {},
+  getters: {},
+  modules: {},
+});
