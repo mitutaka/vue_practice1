89f36ae55bc8d5897010653618f9da770bdc143d
hanada <90606895+mitutaka@users.noreply.github.com>
2022-07-05 23:55:41 +0900

    section.152終了

---
+++ section10/vue3-test/src/views/Children.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Children (子)</div>
+  <Grand-Children />
+</template>
+
+<script>
+import GrandChildren from "@/components/GrandChildren";
+
+export default {
+  components: {
+    GrandChildren,
+  },
+};
+</script>
+
+<style></style>
