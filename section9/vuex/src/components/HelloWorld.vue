bb399c3a2b62ddcb04cf57cd9cc13926ad367da2
hanada <90606895+mitutaka@users.noreply.github.com>
2022-07-05 12:27:56 +0900

    section9.vuex完了

--- section9/vuex/src/components/HelloWorld.vue
+++ section9/vuex/src/components/HelloWorld.vue
@@ -1,18 +1,44 @@
 <template>
   <div>
-    <button @click="addCount">+</button>
+    <!-- <button @click="increment">+</button> -->
+    <button @click="incrementAction">+</button>
+    <button @click="addCount">+10</button>
   </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
   name: "HelloWorld",
   props: {
     msg: String,
   },
   methods: {
+    // increment() {
+    //   this.$store.commit("increment");
+    // },
+    // addCount() {
+    //   this.$store.commit("addCount", {
+    //     value: 10,
+    //   });
+    // },
+    ...mapActions(["incrementAction", "addCountAction"]),
+    // increment() {
+    //   this.$store.dispatch("incrementAction");
+    // },
+    // addCountAction() {
+    //   this.$store.dispatch("addCountAction");
+    // },
+    // addCount() {
+    //   this.$store.dispatch("addCountAction", {
+    //     value: 10,
+    //   });
+    // },
     addCount() {
-      this.$store.commit("increment");
+      this.addCountAction({
+        value: 10,
+      });
     },
   },
 };
--- section9/vuex/src/store/index.js
+++ section9/vuex/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import auth from "./auth";
 
 Vue.use(Vuex);
 
@@ -7,13 +8,44 @@ export default new Vuex.Store({
   state: {
     // 初期値
     count: 0,
+    users: [
+      { id: 1, name: "大谷", isVisible: true },
+      { id: 2, name: "ダルビッシュ", isVisible: false },
+      { id: 3, name: "錦織", isVisible: true },
+    ],
   },
+  getters: {
+    // visibleUsers(state) {
+    //   return state.users.filter((user) => user.isVisible);
+    // },
+    visibleUsers: (state) => state.users.filter((user) => user.isVisible),
+
+    getUserById: (state) => (id) => {
+      return state.users.find((user) => user.id === id);
+    },
+  },
+
   mutations: {
     increment(state) {
       state.count++;
     },
+    addCount(state, payload) {
+      // 第2引数はオブジェクト
+      state.count += payload.value;
+    },
   },
-  actions: {},
-  getters: {},
-  modules: {},
+  actions: {
+    // incrementAction(context) {
+    //   context.commit("increment");
+    // },
+    incrementAction({ commit }) {
+      commit("increment");
+    },
+    addCountAction({ commit }, payload) {
+      commit("addCount", payload);
+    },
+  },
+  modules: {
+    auth,
+  },
 });
